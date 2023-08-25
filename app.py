185d72648c95ddc37ea862900291b0c6aea533d3
Yuri <yurijamesmail@gmail.com>
2023-08-25 11:52:45 +0300

    flask running

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/authenticate")
+def signin():
+    return render_template("login-signup.html")
+if __name__ == "__main__":
+    app.run()
