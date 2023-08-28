3d1edcafb1d2bc1ac9dcdff70bb5185b9e9d47e6
printfCAT <117769903+printfCAT@users.noreply.github.com>
2023-08-28 14:55:56 +0300

    documentation update to main.py

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    """ returns user to index page """
     return render_template("index.html")
+
 
 @app.route("/login")
 def login():
+    """ returns user to login page """
     return render_template("login.html")
+
+
 @app.route("/search")
 def search():
-    return render_template('search.html')
+    """ returns user to search page """
+    return render_template("search.html")
+
 
 if __name__ == "__main__":
     app.run()
