937ef6922b1eeab41a74f79afdf52698045095fa
printfCAT <cliffomanyo@gmail.com>
2023-09-06 20:33:09 +0300

    fixed import error for validation module

--- main.py
+++ main.py
@@ -1,19 +1,58 @@
 #!/usr/bin/python3
 """ retrieve articles and render to webpage """
 
-
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from app.controllers.article_controller import get_article
-from utils.validation import check_uniqueid_in_db, insert_user_to_db
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
 
+# Define the database connection
+engine = create_engine('mysql://group1:med-info1@localhost/health_web_service', echo=True)
+
+# Define a Base class
+Base = declarative_base()
+
+# Define the User model
+class User(Base):
+    __tablename__ = 'uniqueid'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    uniqueid = Column(String(6))
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def insert_user_to_db(uniqueid):
+    """ Create a new User instance and add to the session """
+    try:
+        new_user = User(uniqueid=uniqueid)
+        session.add(new_user)
+        session.commit()
+        return "User added successfully"
+    except Exception as e:
+        session.rollback()
+        return "Error adding user: " + str(e)
+    finally:
+        session.close()
+
+def check_uniqueid_in_db(uniqueid):
+    """ Check if the uniqueid exists in the database """
+    try:
+        session.query(User).filter_by(uniqueid=uniqueid).one()
+        return True
+    except NoResultFound:
+        return False
+    finally:
+        session.close()
 
 @app.route("/")
 def index():
     """ returns user to index page """
     return render_template("index.html")
-
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -23,8 +62,7 @@ def login():
         insert_user_to_db(uniqueid)
         return redirect(url_for('search'))
     else:
-       return render_template("login.html")
-
+        return render_template("login.html")
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
@@ -38,7 +76,6 @@ def signup():
     else:
         return render_template("signup.html")
 
-
 @app.route("/search", methods=['POST', 'GET'])
 def search():
     """ Loads search page and displays results """
@@ -46,7 +83,7 @@ def search():
         symptom = request.form['query']
         url = 'https://health.gov/myhealthfinder/api/v3/itemlist.json?Type=topic'
         articles = get_article(url, symptom)
-        return render_template("search.html", articles=articles) 
+        return render_template("search.html", articles=articles)
     else:
         return render_template("search.html")
 
