f81ae1ded10b759b5a1cecd0d25f522c044a05ab
printfCAT <cliffomanyo@gmail.com>
2023-09-07 18:32:05 +0300

    fixed login/logout features

--- main.py
+++ main.py
@@ -1,80 +1,71 @@
 #!/usr/bin/python3
 """ retrieve articles and render to webpage """
 
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 from app.controllers.article_controller import get_article
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.exc import NoResultFound
+import pymysql
 
 app = Flask(__name__)
 
-# Define the database connection
-engine = create_engine('mysql://group1:med-info1@localhost/health_web_service', echo=True)
+app.secret_key = 'med-info-secret-key'
 
-# Define a Base class
-Base = declarative_base()
-
-# Define the User model
-class User(Base):
-    __tablename__ = 'uniqueid'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    uniqueid = Column(String(6))
-
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
-
-def insert_user_to_db(uniqueid):
-    """ Create a new User instance and add to the session """
-    try:
-        new_user = User(uniqueid=uniqueid)
-        session.add(new_user)
-        session.commit()
-        return "User added successfully"
-    except Exception as e:
-        session.rollback()
-        return "Error adding user: " + str(e)
-    finally:
-        session.close()
-
-def check_uniqueid_in_db(uniqueid):
-    """ Check if the uniqueid exists in the database """
-    try:
-        session.query(User).filter_by(uniqueid=uniqueid).one()
-        return True
-    except NoResultFound:
-        return False
-    finally:
-        session.close()
+db = pymysql.connect(host='localhost', user='group1', password='med-info1', db='web_health_service', cursorclass=pymysql.cursors.DictCursor)
 
 @app.route("/")
 def index():
     """ returns user to index page """
-    return render_template("index.html")
-
-@app.route("/signup", methods=["POST", "GET"])
-def signup():
-    """ handles user login """
-    if request.method == "POST":
-        uniqueid = request.form['uniqueid']
-        insert_user_to_db(uniqueid)
-        return redirect(url_for('login'))
+    if 'logged_in' in session and session['logged_in']:
+        return redirect(url_for('logged_index'))
     else:
-        return render_template("signup.html")
+        return render_template("index.html")
+
+
+@app.route("/loggedindex")
+def logged_index():
+    """ returns user to logged index page """
+    return render_template("loggedindex.html")
+
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    """ handles user login """
+    if request.method == "POST":
+        uniqueid = request.form['uniqueid']
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM uniqueid WHERE uniqueid = %s", (uniqueid,))
+        user = cursor.fetchone()
+        cursor.close()
+        if user:
+            if user:
+                # User found in the database, set the session variable to indicate logged in
+                session['logged_in'] = True
+                return redirect(url_for('logged_index'))
+            else:
+                return "Unique ID not found"
+        else:
+            return "Unique ID not found"
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
     """ Handles user login """
     if request.method == "POST":
         uniqueid = request.form['uniqueid']
-        if check_uniqueid_in_db(uniqueid):
-            return redirect(url_for('search'))
-        else:
-            return "Unique ID not found"
-    else:
-        return render_template("login.html")
+        cursor = db.cursor()
+        try:
+            cursor.execute("INSERT INTO uniqueid (uniqueid) VALUES (%s)", (uniqueid,))
+            db.commit()
+            return "Registration Successful"
+        except Exception as e:
+            db.rollback()
+            return "Error: " + str(e)
+        finally:
+            cursor.close()
+    return render_template("signup.html")
+
 
 @app.route("/search", methods=['POST', 'GET'])
 def search():
@@ -83,9 +74,16 @@ def search():
         symptom = request.form['query']
         url = 'https://health.gov/myhealthfinder/api/v3/itemlist.json?Type=topic'
         articles = get_article(url, symptom)
-        return render_template("search.html", articles=articles)
+        return render_template("search.html", articles=articles) 
     else:
         return render_template("search.html")
+
+
+@app.route("/logout")
+def logout():
+    """ Logout the user by clearing the session """
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
